ed0740c80a5d978b2220d84322d7edfc6da96932
junluck <junaindavidsvickerman@gmail.com>
2024-07-16 19:30:18 +0200

    Update javascript comments

--- editdoo.js
+++ editdoo.js
@@ -6,19 +6,19 @@ const sliderArrowOne = document.querySelector(".rightArrowButton")
 const sliderArrowTwo = document.querySelector('.leftArrowButton');
 const menuOne = document.querySelector('.orderN');
 const menuTwo = document.querySelector('.orderNTwo');
-//addeventlistener enable active class once element is clicked
+//addeventlistener enable active class once element is clicked turning burger menu into an x
 humburger.addEventListener('click', () =>{
     humburger.classList.toggle('active');
     hiddenMenu.classList.toggle('active');
 })
-//addeventlistener enable active class once element is clicked
+//addeventlistener enable active class once element is clicked moving the packge section
 sliderArrowTwo.addEventListener('click',() =>{
     menuOne.classList.toggle('active');
     menuTwo.classList.toggle('active');
     
 
 })
-//addeventlistener enable active class once element is clicked
+//addeventlistener enable active class once element is clicked moving the packge section
 sliderArrowOne.addEventListener('click',() =>{
     menuOne.classList.toggle('active');
     menuTwo.classList.toggle('active');
