912eda23d41f020726d9ba3811bca1b393ad9afc
junluck <junaindavidsvickerman@gmail.com>
2024-08-11 23:55:45 +0200

    update skider

--- editdoo.js
+++ editdoo.js
@@ -37,15 +37,19 @@ const pauseButton = document.querySelector('.pauseButton');
 //storing video element in variable
 const phoneVideo = document.querySelector('.phoneVideo');
 const animationOne = document.querySelector('.animationOne');
-
+const videoOne = document.querySelector(".videoOne");
+const videoTwo = document.querySelector(".videoTwo");
+let activeNumber = 0;
 
 
 class Video {
-    constructor(video, duration){
+    constructor(video, duration, videoName){
         this._name = video;
         this._duration = duration;
-        this._startTimer = document.getElementById('timerStart');
-        this._endTimer = document.getElementById('timerEnd');
+        this._startTimer = videoName.querySelector('.timerStart');
+        this._endTimer = videoName.querySelector('.timerEnd');
+        this._videoName = videoName;
+        this._circle  = videoName.querySelector(".circle")
     }
         get duration(){
             return this._duration
@@ -62,17 +66,26 @@ class Video {
         get endTimer(){
             return this._endTimer
         }
+
+        get videoName(){
+            return this._videoName
+        }
+
+        get circle(){
+            return this._circle
+        }
+    
     
 }
-const videoOneObject = new Video(phoneVideo, 18.87);
-const videoTwoObject = new Video(animationOne, 154);
+const videoOneObject = new Video(phoneVideo,19,videoOne);
+const videoTwoObject = new Video(animationOne,114,videoTwo);
 //group videos up and sort in array of objects
 const groupOfVideos = [videoOneObject, videoTwoObject]
 
 //store playback bar and  playback button in variables
-const whiteBarline = document.querySelector('.timeLineBarWhite');
-const blueBarline = document.querySelector('.timeLineBarBlue');
-const circle = document.querySelector('.circle');
+const whiteBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarWhite');
+const blueBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarBlue');
+const circle = groupOfVideos[activeNumber].circle
 let circleCor = circle.getBoundingClientRect().right;
 let whiteBarlineCor = whiteBarline.getBoundingClientRect().right;
 let whiteBarlineCorLeft = whiteBarline.getBoundingClientRect().left;
@@ -89,6 +102,7 @@ window.addEventListener('resize', () => {
 let mouseCordinates = 0
 
 //declaring a function that will upadte time of video and alow user to move play head to any point on timeline
+
 function updateEndDuration(video){
     let videoDuration = video.duration;
 
@@ -117,6 +131,9 @@ function updateEndDuration(video){
 
     let  interValTimer = undefined;
     video.name.addEventListener("playing", (event) => {
+        const whiteBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarWhite');
+        const blueBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarBlue');
+        const circle = groupOfVideos[activeNumber].circle
         playButton.style.display = 'none';
         pauseButton.style.display = 'flex';
         interValTimer = setInterval(() => {
@@ -127,11 +144,19 @@ function updateEndDuration(video){
         }
         
         let num =  video.name.currentTime;
-        let currentTime = convertToSecFormat(num);
+        let convertedNum = video.name.currentTime
+        if (video.name.currentTime >= 60){
+            convertedNum  = 100 + (video.name.currentTime - 60)
+        }
+        
+        let currentTime = convertToSecFormat(convertedNum);
         video.startTimer.innerHTML = `${currentTime}`;
-        let percentage = (num / videoDuration) * 100;
+        let percentage = (video.name.currentTime / video.duration) * 100;
         circle.style.left = `${percentage - 2}%`;
         blueBarline.style.width= `${percentage}%`;
+        console.log(video.duration)
+        console.log(video.name.currentTime)
+        console.log(video.name.currentTime)
         
 
             
@@ -144,19 +169,16 @@ function updateEndDuration(video){
       clearInterval(interValTimer)
     })
     let endNumber = convertToSecFormat(videoDuration)
-    video.endTimer.innerHTML = endNumber;
-
-    const whiteBarline = document.querySelector('.timeLineBarWhite');
-    const blueBarline = document.querySelector('.timeLineBarBlue');
-    const circle = document.querySelector('.circle');
-    const circleCor = circle.getBoundingClientRect().right;
-    const whiteBarlineCor = whiteBarline.getBoundingClientRect().right;
+    //video.endTimer.innerHTML = endNumber;
     let mouseCordinates = 0
     
-    document.querySelector(".playBarLine").addEventListener('click',(event) => {
+    video.videoName.querySelector(".playBarLine").addEventListener('click',(event) => {
+    const whiteBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarWhite');
+    const blueBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarBlue');
+    const circle = groupOfVideos[activeNumber].circle
     mouseCordinates = ((event.clientX - whiteBarline.getBoundingClientRect().left) / (whiteBarline.getBoundingClientRect().right - whiteBarline.getBoundingClientRect().left)) * 100;
-    groupOfVideos[0].name.currentTime = groupOfVideos[0].duration *(mouseCordinates/ 100);
-    let currentTime = convertToSecFormat(groupOfVideos[0].duration *(mouseCordinates/ 100));
+    groupOfVideos[activeNumber].name.currentTime = groupOfVideos[activeNumber].duration *(mouseCordinates/ 100);
+    let currentTime = convertToSecFormat(groupOfVideos[activeNumber].duration *(mouseCordinates/ 100));
     let cordinates = mouseCordinates;
     console.log(screenWidth)
     if (screenWidth <= 1000){
@@ -181,102 +203,111 @@ function updateEndDuration(video){
         
     })
 
-}
+    //groupOfVideos[0].name.muted = true;
 
-//invoking the function onto the active video on the screen
-
-
-groupOfVideos[0].name.volume = 0.01
-const volume = document.querySelector(".volume");
+    let isMouseDown = false;
+    let isMouseDownTwo = false
 
 
+    video.circle.addEventListener('mousedown', () =>{
+        isMouseDown = true;
+        phoneVideo.pause();
+        playButton.style.display = 'flex';
+        pauseButton.style.display = 'none';
+    })
 
-
-
-//groupOfVideos[0].name.muted = true;
-
-let isMouseDown = false;
-let isMouseDownTwo = false
-
-
-circle.addEventListener('mousedown', () =>{
-    isMouseDown = true;
-    phoneVideo.pause();
-    playButton.style.display = 'flex';
-    pauseButton.style.display = 'none';
-})
-
-document.addEventListener('mouseup', () =>{
-    isMouseDown = false;
-    if (isMouseDownTwo){
-       
-    }
-
-})
-
-circle.addEventListener('mouseup', () =>{
-    //playButton.style.display = 'none';
-    //pauseButton.style.display = 'flex';
-    isMouseDownTwo = true;
-    
-    
-})
-
-
-document.querySelector(".playBarLine").addEventListener('mousemove', (event) =>{
-    let percentage = (event.clientX - whiteBarline.getBoundingClientRect().left) / (whiteBarline.getBoundingClientRect().right - whiteBarline.getBoundingClientRect().left) * 100;
-    if (percentage < 0){
-        percentage = 0;
-    }
-    else if (percentage > 100){
-        percentage = 100;
-    }
-    if (isMouseDown){
-        console.log(percentage);
-        circle.style.left = `${percentage - 2}%`;
-        blueBarline.style.width =`${percentage}%`;
-        groupOfVideos[0].name.currentTime = groupOfVideos[0].duration * percentage / 100;
+    document.addEventListener('mouseup', () =>{
+        isMouseDown = false;
+        if (isMouseDownTwo){
         
+        }
+
+    })
+
+    video.circle.addEventListener('mouseup', () =>{
+        //playButton.style.display = 'none';
+        //pauseButton.style.display = 'flex';
+        isMouseDownTwo = true;
+        
+        
+    })
+
+
+    video.videoName.querySelector(".playBarLine").addEventListener('mousemove', (event) =>{
+        const whiteBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarWhite');
+        const blueBarline =  groupOfVideos[activeNumber].videoName.querySelector('.timeLineBarBlue');
+        const circle = groupOfVideos[activeNumber].circle
+        let percentage = (event.clientX - whiteBarline.getBoundingClientRect().left) / (whiteBarline.getBoundingClientRect().right - whiteBarline.getBoundingClientRect().left) * 100;
+        if (percentage < 0){
+            percentage = 0;
+        }
+        else if (percentage > 100){
+            percentage = 100;
+        }
+        if (isMouseDown){
+            console.log(percentage);
+            circle.style.left = `${percentage - 2}%`;
+            blueBarline.style.width =`${percentage}%`;
+            groupOfVideos[activeNumber].name.currentTime = groupOfVideos[activeNumber].duration * percentage / 100;
+            
+        }
+
+    })
+
     }
 
-})
+    //invoking the function onto the active video on the screen
 
 
-function volumEditor(video){
+    groupOfVideos[activeNumber].name.volume = 0.01
+    const volume = document.querySelector(".volume");
+
+
+
+
+
+
+
+
+    function volumEditor(video){
+        
+        let rangeValue = video.videoName.querySelector(".volume");
+        let clicker = true;
+        let progress = video.videoName.querySelector(".volumeBlueBar")
+
+        rangeValue.oninput = function(){
+            progress.value = rangeValue.value;
+            video.name.volume = progress.value / 100;
+        }
+
+        progress.addEventListener("click", (event)=>{
+            let sizeOfBar = rangeValue.getBoundingClientRect().right -  rangeValue.getBoundingClientRect().left ;
+            let mousePosition = event.clientX - rangeValue.getBoundingClientRect().left;
+            let mousePercent = mousePosition / sizeOfBar  * 100;
+            rangeValue.value = mousePercent;
+            progress.value = mousePercent;
+            video.name.volume = progress.value / 100;
+            console.log(rangeValue.getBoundingClientRect().right)
+            console.log(mousePercent );
+        })
+
+        rangeValue.addEventListener("input", () =>{
+            console.log(rangeValue.value)
+        })
     
-    let rangeValue = document.querySelector(".volume");
-    let clicker = true;
-    let progress = document.querySelector(".volumeBlueBar")
-
-    rangeValue.oninput = function(){
-        progress.value = rangeValue.value;
-        video.name.volume = progress.value / 100;
-    }
-
-    progress.addEventListener("click", (event)=>{
-        let sizeOfBar = rangeValue.getBoundingClientRect().right -  rangeValue.getBoundingClientRect().left ;
-        let mousePosition = event.clientX - rangeValue.getBoundingClientRect().left;
-        let mousePercent = mousePosition / sizeOfBar  * 100;
-        rangeValue.value = mousePercent;
-        progress.value = mousePercent;
-        video.name.volume = progress.value / 100;
-        console.log(rangeValue.getBoundingClientRect().right)
-        console.log(mousePercent );
-    })
-
-    rangeValue.addEventListener("input", () =>{
-        console.log(rangeValue.value)
-    })
-   
 }
+
+
+
 
 //let activeVideo = groupOfVideos[activeNumber]
 
 function activeVideoInloop(arrayOfVideos){
     let rewindButton = document.querySelector(".rewind");
     let fastForwardButton = document.querySelector(".fastForward");
-    let activeNumber = 0;
+    let previousActiveNumber = 0
     rewindButton.addEventListener("click", ()=>{
+        previousActiveNumber = activeNumber;
         activeNumber -= 1;
         if (activeNumber < 0){
             activeNumber = arrayOfVideos.length - 1;
@@ -285,8 +316,15 @@ function activeVideoInloop(arrayOfVideos){
             activeNumber = 0;
         }
         console.log(activeNumber);
+        arrayOfVideos[previousActiveNumber].name.pause()
+        playButton.style.display = 'flex';
+        pauseButton.style.display = 'none';
+        arrayOfVideos[activeNumber].name.style.transform= `scale(1.02)`
+        arrayOfVideos[activeNumber].videoName.style.right = `0`
+        arrayOfVideos[activeNumber - 1].videoName.style.opacity = `0%`
     })
     fastForwardButton.addEventListener("click", ()=>{
+        previousActiveNumber = activeNumber;
         activeNumber += 1;
         if (activeNumber < 0){
             activeNumber = arrayOfVideos.length - 1;
@@ -294,7 +332,10 @@ function activeVideoInloop(arrayOfVideos){
         else if(activeNumber >=arrayOfVideos.length){
             activeNumber = 0;
         }
+        arrayOfVideos[previousActiveNumber].name.pause()
         console.log(activeNumber);
+        playButton.style.display = 'flex';
+        pauseButton.style.display = 'none';
     })
 
     //add event listener to play button and play video when clicked
@@ -302,8 +343,8 @@ function activeVideoInloop(arrayOfVideos){
         arrayOfVideos[activeNumber].name.play();
         playButton.style.display = 'none';
         pauseButton.style.display = 'flex';
-        
-        
+        updateEndDuration(arrayOfVideos[activeNumber]);
+        volumEditor(arrayOfVideos[activeNumber]);
     })
 
 //add event listener to pause button and pause video when clicked
@@ -311,11 +352,14 @@ function activeVideoInloop(arrayOfVideos){
         arrayOfVideos[activeNumber].name.pause();
         playButton.style.display = 'flex';
         pauseButton.style.display = 'none';
+        updateEndDuration(arrayOfVideos[activeNumber]);
+        volumEditor(arrayOfVideos[activeNumber]);
+
         
     })
+
     updateEndDuration(arrayOfVideos[activeNumber]);
     volumEditor(arrayOfVideos[activeNumber]);
-
 }
 
 activeVideoInloop(groupOfVideos);
