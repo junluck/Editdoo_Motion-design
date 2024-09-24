8320d63c463dfd401d99687941da5d6e49ca5493
junluck <junaindavidsvickerman@gmail.com>
2024-09-24 19:37:43 +0200

    update package page

---
+++ contactForm/contact.js
@@ -0,0 +1,32 @@
+import navbar from "/navbar.js";
+
+navbar()
+
+
+const form = document.querySelector(".forms")
+const thankYou = document.querySelector(".thankYou")
+
+form.addEventListener("submit",(event)=>{
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData)
+    console.log(data)
+    event.preventDefault()
+    function sendMail(){
+        let params = data
+
+        console.log(emailjs)
+
+        emailjs.send("service_ouim9y7","template_fj35ky5", params).then(()=>{
+            form.style.display = "none"
+        }).catch((e)=>{
+            console.log("error has occured")
+        })
+        }
+        
+        sendMail()
+    
+
+        form.style.display = "none";
+        thankYou.style.display = "block"
+    })
+    
