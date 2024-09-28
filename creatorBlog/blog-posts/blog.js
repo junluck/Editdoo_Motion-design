3e8981c731216fd5857dc5167c3b4e6c5b3a4cab
junluck <junaindavidsvickerman@gmail.com>
2024-09-28 12:33:36 +0200

    update comments

--- creatorBlog/blog-posts/blog.js
+++ creatorBlog/blog-posts/blog.js
@@ -4,9 +4,32 @@ navbar()
 
 const form = document.getElementById("commentForm");
 const nameId = document.getElementById("name");
-const messageId = document.getElementById("message")
-const commentsDiv = document.getElementById("comments")
-const arrayOfComments = []
+const messageId = document.getElementById("message");
+const commentsDiv = document.getElementById("comments");
+const arrayOfComments = [];
+let arrayOfCom= JSON.parse(localStorage.getItem('arrayInMem'));
+console.log(arrayOfCom)
+
+function appender(ele){
+    const newDiv = document.createElement("div");
+    const name = document.createElement("h4");
+    const comment =  document.createElement("p");
+    name.textContent = ele.username;
+    comment.textContent = ele.message;
+    newDiv.className = "individualComment";
+    newDiv.appendChild(name);
+    newDiv.appendChild(comment);
+    commentsDiv.appendChild(newDiv)
+}
+
+function printOutCommennts(){
+    arrayOfCom.map((element)=>{
+        appender(element)
+    })
+
+}
+
+printOutCommennts();
 
 function renderCommentInArray(array){
     let endOfArray = array.length - 1 
@@ -14,24 +37,14 @@ function renderCommentInArray(array){
 }
 
 form.addEventListener("submit",(event)=>{
-    let newArray = [{id:"9603255029086",name:"junain Davids"}]
-    console.log(nameId.value);
+    let arrayInMemory = JSON.parse(localStorage.getItem('arrayInMem')) || [];
+    let newComment = {username:nameId.value,message:messageId.value};
+    arrayInMemory.push(newComment);
+    localStorage.setItem("arrayInMem",JSON.stringify(arrayInMemory))
     event.preventDefault();
-    const newDiv = document.createElement("div");
-    const name = document.createElement("h4");
-    const comment =  document.createElement("p");
-    name.textContent = nameId.value;
-    comment.textContent = messageId.value;
-    newDiv.className = "individualComment";
-    newDiv.appendChild(name);
-    newDiv.appendChild(comment);
-    arrayOfComments.push(newDiv);
-    localStorage.setItem("myArray",JSON.stringify(newArray))
-    const comments = JSON.parse(localStorage.getItem('myArray'))
-    console.log(comments[0].id)
-    renderCommentInArray(arrayOfComments);
-    nameId.value = ""
-    messageId.value = ""
+    appender(newComment);
+    nameId.value = "";
+    messageId.value = "";
     
 
 })
